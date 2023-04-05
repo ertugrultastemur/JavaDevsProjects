a973fdbdfec88c69e1bc8e775880e52f881c99e8
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-04-05 06:30:04 +0300

    funcions are updated.
    conditions have been added and made operational.

--- Devs/src/main/java/kodlama/io/Devs/dataAccess/concretes/InMemoryPLangRepository.java
+++ Devs/src/main/java/kodlama/io/Devs/dataAccess/concretes/InMemoryPLangRepository.java
@@ -10,37 +10,72 @@ import kodlama.io.Devs.dataAccess.abstracts.IPLangRepository;
 import kodlama.io.Devs.entities.concretes.PLang;
 
 @Repository
-public class InMemoryPLangRepository implements IPLangRepository{
+public class InMemoryPLangRepository implements IPLangRepository {
 	PLang pLang;
 	List<PLang> pLangs;
 	
 	@Autowired
-	public InMemoryPLangRepository() {
+	public InMemoryPLangRepository() throws Exception{
 		
 		pLangs = new ArrayList<PLang>();
 		add(0,"java");
-		add(1,"c++");
+		add(1,"c#");
 		add(2,"python");
-		update(1, "C#");
+		update(0, "c++");
+		getById(1);
 		
 		
 	}
 	
 
 	@Override
-	public void add(int id, String name) {
+	public void add(int id, String name) throws Exception{
+		boolean isValid=true;
+		for (PLang pLang : pLangs) {
+			if(name.isEmpty()) {
+				isValid=false;
+				System.out.println("This area cannot be empty.");
+				break;
+			}
+		}
+		for (PLang pLang : pLangs) {
+				if(name == pLang.getName()) {
+					isValid =false;
+					System.out.println("This name already exists.");
+					break;
+				}
+		}
+		if(isValid==true) pLangs.add(new PLang(id,name));
 		
-		pLangs.add(new PLang(id,name));
 	}
 
 	@Override
 	public void update(int id,String name) {
+		boolean isValid=true;
 		for (PLang pLang : pLangs) {
-			if(pLang.getId()==id) {
-				pLang.setName(name);
+			if(name.isEmpty()) {
+				isValid=false;
+				System.out.println("This area cannot be empty.");
+				break;
 			}
+			if(name == pLang.getName()) {
+				isValid=false;
+				System.out.println("This name already exists.");
+			}
+			
+			
+		
 		}
-		System.out.println("InMemory : Güncellendi");
+		
+		if (isValid==true) {
+			for (PLang pLang : pLangs) {
+				if (pLang.getId() == id && name != pLang.getName()) {
+					pLang.setName(name);
+					break;
+				}
+			}
+			System.out.println("InMemory : Updated");
+		}
 
 	}
 
@@ -52,9 +87,7 @@ public class InMemoryPLangRepository implements IPLangRepository{
 	@Override
 	public List<PLang> getAll() {
 		return pLangs;
-//		for (PLang pLang : pLangs) {
-//			pLangs.get(pLang.getId());
-//		}
+
 	}
 
 	@Override
