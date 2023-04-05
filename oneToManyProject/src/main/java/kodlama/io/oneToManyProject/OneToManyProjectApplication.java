2f5ee5a30f2e7a8f44121d6cf9e925c6654e6503
Ertuğrul Taştemür <34662874+ertugrultastemur@users.noreply.github.com>
2023-04-05 19:06:19 +0300

    new project added.

---
+++ oneToManyProject/src/main/java/kodlama/io/oneToManyProject/OneToManyProjectApplication.java
@@ -0,0 +1,13 @@
+package kodlama.io.oneToManyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyProjectApplication.class, args);
+	}
+
+}
